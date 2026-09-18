8fb80f66107749ff1ed22f7365113e2b52398b11
Valentina Alves <cv3819820@gmail.com>
2026-09-18 09:41:01 -0300

    add operador atribuicao modulo js

---
+++ 11-operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDolces = 10;
+let crianca = 3;
+
+//queremos saber quantos doces sobram após dividir igaulmente
+totalDolces %= crianças;
+
+console.log(totalDolces);// 1-> Pois 10 dividido por 3 sobra 1
