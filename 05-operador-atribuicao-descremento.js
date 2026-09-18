d7af51450110164cbe1dc64e61ac64f3d7031495
Valentina Alves <cv3819820@gmail.com>
2026-09-18 09:25:29 -0300

    add operador

---
+++ 05-operador-atribuicao-descremento.js
@@ -0,0 +1,3 @@
+let vidas  
+vidas--; //0 mesmo que vidas = vidas - 1
+console.log(vidas);// 2
---
+++ tipos-de-dados-number.js
@@ -0,0 +1,10 @@
+let idade = 25;
+let altura = 1.75;
+let temperatura = -5
+console.log(idade)
+console.log(altura)
+console.log(temperatura)
+
+console.log("Minha idade é", idade)
+console.log("Minha altura é", altura)
+console.log("Minha temperatura é ", temperatura)
