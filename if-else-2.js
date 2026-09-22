a4b79c6abf732cc4ba0aa16761dc23103faf4f6a
camily-vitoria-braga <cv3819820@gmail.com>
2026-09-22 08:39:21 -0300

    add if else

---
+++ if-else-2.js
@@ -0,0 +1,6 @@
+let diaDaSemana = "quarta-feira"
+
+if (diaDaSemana === "sábado" || diaDaSemana === "domingo"){
+}else {
+    console.log("È fim de semana!");
+}
---
+++ if.js
@@ -0,0 +1,5 @@
+let hora = 15
+if (hora < 12){
+    console.log("Bom dia ");
+}// o restante do código continua independente se a condiçao for verdadeira ou falsa.
+console.log("Dinheiro não traz felicidade. me de~o seu e seja feliz.")
